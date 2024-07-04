99562ed5041e716004b00125834d994b37aa891e
lsm <sm00216@naver.com>
2024-07-04 21:48:17 +0900

    next.config.js 수정

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,9 @@ const nextConfig = {
                 hostname:  'images.unsplash.com'
             }
         ]
+    },
+    experimental: {
+        missingSuspenseWithCSRBailout: false
     }
 };
 
